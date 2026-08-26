f57e42f54c5c8ceec6013675714603ad85564d15
Josh <jkline378@students.mchenry.edu>
2026-08-26 11:45:44 -0500

    Add Data Types example for assignment requirements

---
+++ Data-types.py
@@ -0,0 +1,25 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+
+"""
+ADD-100: Data Types (Video game weapons shop)
+"""
+item_name = "sword"  # String
+quanity = 1  # Integer
+price = 200  # Float
+On_sale = False  # Boolean
+print(f"Item: {item_name}")
+print(f"Quanity: {quanity}")
+print(f"Price: {price}")
+print(f"Sale: {On_sale}")
